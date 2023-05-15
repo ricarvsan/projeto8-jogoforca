33df27b933455b875f7e2deb885a945c9b6120c5
ricarvsan <ricarvsan@gmail.com>
2023-05-15 13:59:30 -0300

    semi finalizado, falta componentização e ajuste de data-tests

--- src/App.js
+++ src/App.js
@@ -6,34 +6,96 @@ import forca4 from './assets/forca4.png';
 import forca5 from './assets/forca5.png';
 import forca6 from './assets/forca6.png';
 import palavras from './palavras';
+import { useState } from 'react';
 
 
 export default function App() {
 
-  let listaPalavras = palavras.sort(() => Math.random() - 0.5);
   const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  let palavraEscolhida = '';
+  const imgsForca = [forca0, forca1, forca2, forca3, forca4, forca5, forca6];
+  let [imgForca, setImgForca] = useState(imgsForca[0]);
+  let [palavra, setPalavra] = useState(['']);
+  let [palavraEscolhida, setPalavraEscolhida] = useState('');
+  let [btnClicados, setBtnClicados] = useState(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]);
+  let [qtdErros, setQtdErros] = useState(0);
+  let [corPalavra, setCorPalavra] = useState('letraPalavra')
 
-  console.log(listaPalavras)
+  function btnClicado(letra) {
+    let novoBtnClicados = [...btnClicados, letra];
+    setBtnClicados(novoBtnClicados);
+
+    if(palavraEscolhida.includes(letra)) {
+      let novaPalavra = [...palavra];
+      for(let i = 0; i < palavraEscolhida.length; i++) {
+        if(palavraEscolhida[i] === letra) {
+          novaPalavra[i] = letra;
+        }
+      }
+      setPalavra(novaPalavra);
+      if(novaPalavra.includes('_')){
+        console.log('ainda nao acabo');
+      } else {
+        setCorPalavra('letraPalavra ganhou');
+        setBtnClicados(alfabeto);
+      }
+    } else {
+      let novaQtdErro = qtdErros + 1;
+      setQtdErros(novaQtdErro);
+      setImgForca(imgsForca[novaQtdErro]);
+
+      if(novaQtdErro === 6) {
+        let novaPalavra = [...palavra];
+        for(let i = 0; i < palavraEscolhida.length; i++) {
+          novaPalavra[i] = palavraEscolhida[i];
+        }
+        setPalavra(novaPalavra);
+        setBtnClicados(alfabeto);
+        setCorPalavra('letraPalavra perdeu');
+      }
+    }
+  }
+  
+  function criaPalavraEmBranco(palavra) {
+    let palavraEmBranco = [];
+    for(let i = 0; i < palavra.length; i++) {
+      palavraEmBranco.push('_');
+    }
+  
+    return palavraEmBranco;
+  }
+  
+  function iniciaJogo() {
+    //RESETA BOTOES CLIDADOS
+    setBtnClicados([]);
+    setQtdErros(0);
+    setImgForca(imgsForca[0]);
+    setCorPalavra('letraPalavra');
+
+    let [palavraEscolhida] = palavras.sort(() => Math.random() - 0.5);
+    setPalavraEscolhida(palavraEscolhida);
+    console.log(palavraEscolhida)
+    let palavraEmBranco = criaPalavraEmBranco(palavraEscolhida);
+    setPalavra(palavraEmBranco);
+  }
 
   return (
     <div>
       <div className='top'>
         <div className='imgForca'>
-          <img className='forca' src={forca6} alt=""></img>
+          <img data-test="game-image" className='forca' src={imgForca} alt=""></img>
         </div>
         <div className='escolherPalavra'>
-          <button className="btnEscolherPalavra">
+          <button data-test="choose-word" className="btnEscolherPalavra" onClick={iniciaJogo}>
             Escolher Palavra
           </button>
           <div className='palavra'>
-            {palavraEscolhida}
+          {palavra.map((letra, indice) => (<div data-test="word" key={indice} className={corPalavra}>{letra}</div>))}
           </div>
         </div>        
       </div>
 
       <div className='letras'>
-          {alfabeto.map((letra) => (<button disabled key={letra} className='btnLetras'>{letra}</button>))}
+          {alfabeto.map((letra) => (<button data-test="letter" onClick={() => btnClicado(letra)} disabled={(btnClicados.includes(letra)) ? true : false} key={letra} className={(btnClicados.includes(letra)) ? `btnLetras` : `btnLetras btnLetrasHabilitado`}>{letra}</button>))}
       </div>
     </div>
   );
